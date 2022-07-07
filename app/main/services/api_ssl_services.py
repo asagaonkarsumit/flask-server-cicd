77b3e63c32f8b4e557f991d45f40bace105b5e34
asagaonkarsumit <sumit@opsfuse.com>
2022-07-07 18:09:51 +0530

    CUR test2 v2

--- app/main/services/api_ssl_services.py
+++ app/main/services/api_ssl_services.py
@@ -17,7 +17,7 @@ from flask import current_app as cur_app
 
 def save_content(data):
 
-    return {"45statuss": "hello kkkkkk"}
+    return {"45statuss": "hello test 2 v2"}
 
 
 def check_ssl_status(fqdn):
